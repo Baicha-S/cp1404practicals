b0a660aff5f7935135185fc6789939aace585c3e
Baicha <pornnatcha.sarujikomjornwattana@my.jcu.edu.au>
2023-11-20 18:06:45 +0800

    Add function to menu_score.py program

---
+++ prac_02/menu_score.py
@@ -0,0 +1,50 @@
+MENU = "(G)et a valid score\n" \
+       "(P)rint result\n" \
+       "(S)how stars\n" \
+       "(Q)uit"
+
+
+def main():
+    score = ""
+    print(MENU)
+    choice = input("Choice > ").upper()
+    while choice != "Q":
+        if choice == "G":
+            score = get_valid_score()
+        elif choice == "P":
+            result = determine_score(score)
+            print(result)
+        elif choice == "S":
+            star = print_star(score)
+            print(star)
+        else:
+            print("Invalid choice")
+        print(MENU)
+        choice = input("Choice > ").upper()
+    print("Farewell")
+
+
+def get_valid_score():
+    score = int(input("Score: "))
+    while score <= 0 or score >= 100:
+        print("Score must be 0-100")
+        score = float(input("Score: "))
+    return score
+
+
+def determine_score(score):
+    if 0 <= score <= 100:
+        if score >= 90:
+            return "Excellent"
+        elif score >= 50:
+            return "Passable"
+        else:
+            return "Bad"
+
+
+def print_star(score):
+    for star in range(0, int(score)):
+        return "*" * score
+
+
+main()
