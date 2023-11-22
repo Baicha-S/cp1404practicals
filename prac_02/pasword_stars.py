386346afbdc6bc038a63dd8b601519798dccb96e
Baicha <pornnatcha.sarujikomjornwattana@my.jcu.edu.au>
2023-11-22 10:44:12 +0800

    Add docstrings to functions.

--- prac_02/pasword_stars.py
+++ prac_02/pasword_stars.py
@@ -1,19 +1,29 @@
+"""
+CP1404 - Programming II
+https://github.com/Baicha-S/cp1404practicals
+password_stars program.
+"""
+PASSWORD_MINIMUM_LENGTH = 6
+
+
 def main():
+    """Main password_stars program."""
     password = get_password()
     print_asterisks(password)
 
 
-def print_asterisks(password):
-    print("*" * len(password))
-
-
 def get_password():
+    """Get user's password and validate password length."""
     password = input("Password: ")
-    password_minimum_length = int(input("Minimum Length: "))
-    while len(password) < password_minimum_length:
+    while len(password) < PASSWORD_MINIMUM_LENGTH:
         print("Password doesn't meet a minimum length")
         password = input("Password: ")
     return password
+
+
+def print_asterisks(password):
+    """Print asterisks as long as password length."""
+    print("*" * len(password))
 
 
 main()
