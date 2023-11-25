da1d95dce939eea9f995a54b8383cf4b8ff480f1
Baicha <pornnatcha.sarujikomjornwattana@my.jcu.edu.au>
2023-11-26 01:01:48 +0800

    Answer to the questions.

---
+++ prac_03/randoms.py
@@ -0,0 +1,16 @@
+import random
+
+print(random.randint(5, 20))  # line 1
+print(random.randrange(3, 10, 2))  # line 2
+print(random.uniform(2.5, 5.5))  # line 3
+
+"""
+What did you see on line 1?
+The smallest number was 5, and the largest was 20
+
+What did you see on line 2?
+The smallest number was 3, and the largest was 9
+
+What did you see on line 3?
+The smallest number was 2.5, and the largest was 5.5
+"""
