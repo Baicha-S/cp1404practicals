1fabdb014552b9afbc4acc57d89f717555145a9a
Baicha <pornnatcha.sarujikomjornwattana@my.jcu.edu.au>
2023-11-27 20:24:19 +0800

    Answer to the questions.

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -5,12 +5,16 @@ print(random.randrange(3, 10, 2))  # line 2
 print(random.uniform(2.5, 5.5))  # line 3
 
 """
-What did you see on line 1?
+1. What did you see on line 1?
 The smallest number was 5, and the largest was 20
 
-What did you see on line 2?
+2. What did you see on line 2?
 The smallest number was 3, and the largest was 9
 
-What did you see on line 3?
+3. What did you see on line 3?
 The smallest number was 2.5, and the largest was 5.5
+
+4. Write code, not a comment, to produce a random number between 1 and 100 inclusive.
 """
+
+print(random.randint(1, 100))
