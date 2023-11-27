6661edac90b7aa4d82b7969df0417ba514eb8259
Baicha <pornnatcha.sarujikomjornwattana@my.jcu.edu.au>
2023-11-27 20:23:45 +0800

    Declare filename as a constant.

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,29 +1,41 @@
-# 1
+"""
+1 Write code that asks the user for their name,
+then opens a file called "name.txt" and writes that name to it.
+Remember to close your file.
+"""
+FILENAME = "name.txt"
 name = input("Name: ")
-out_file = open("name.txt", 'w')
+out_file = open(FILENAME, 'w')
 print(name, file=out_file)
 out_file.close()
 
-# 2
-in_file = open("name.txt", 'r')
+"""
+2. In the same file, but as if it were a separate program) Write code that opens 
+"name.txt" and reads the name (as above) then prints,
+"Your name is Bob" (or whatever the name is in the file).
+"""
+in_file = open(FILENAME, 'r')
 name = in_file.read()
-in_file.close()
 print(f"Your name is {name}")
+in_file.close()
 
 # 3
+FILENAME = "numbers.txt"
 NUMBER_OF_LINE = 2
-in_file = open("numbers.txt", 'r')
+in_file = open(FILENAME, 'r')
 total = 0
 for line in range(NUMBER_OF_LINE):
     number = int(in_file.readline())
     total += number
-in_file.close()
 print(total)
+in_file.close()
 
 # 4
-in_file = open("numbers.txt", 'r')
+FILENAME = "numbers.txt"
 count = 0
+in_file = open("numbers.txt", 'r')
 lines = in_file.readlines()
 for line in lines:
     count += 1
 print(f"{count} lines")
+in_file.close()
