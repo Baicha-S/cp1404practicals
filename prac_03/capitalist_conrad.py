bed037e6eaf039cbe6af72cb6a84aa38f70321e8
Baicha <pornnatcha.sarujikomjornwattana@my.jcu.edu.au>
2023-11-27 20:23:11 +0800

    Edit string format.

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -12,14 +12,16 @@ import random
 MAX_INCREASE = 0.175  # 17.5%
 MAX_DECREASE = 0.05  # 5%
 MIN_PRICE = 1.0
-MAX_PRICE = 100.0
+MAX_PRICE = 1000.0
 INITIAL_PRICE = 10.0
 OUTPUT_FILE = "stock_price.txt"
-
-out_file = open("stock_price.txt", 'w')
 day_count = 0
+
 price = INITIAL_PRICE
 print(f"Starting price: ${price:,.2f}")
+
+out_file = open("stock_price.txt", 'w')
+print(f"Starting price: ${price:,.2f}", file=out_file)
 
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
@@ -37,4 +39,5 @@ while MIN_PRICE <= price <= MAX_PRICE:
     price *= (1 + price_change)
     day_count += 1
     print(f"On day {day_count} price is: ${price:,.2f}", file=out_file)
+    print(f"On day {day_count} price is: ${price:,.2f}")
 out_file.close()
