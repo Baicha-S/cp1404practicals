3132883f18bcd5eab7cde2283c871cf50fb80ec0
Baicha <pornnatcha.sarujikomjornwattana@my.jcu.edu.au>
2023-12-03 00:25:36 +0800

    Complete the list_exercise program.

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,6 +1,7 @@
+PROMPT_INPUT = 5
 numbers = []
 total = 0
-for i in range(0, 5):
+for i in range(0, PROMPT_INPUT):
     number = int(input("Number: "))
     numbers.append(number)
     total += number
@@ -8,4 +9,4 @@ print(f"The first number is {numbers[0]}")
 print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
-print(f"The average of the numbers is {total / 5}")
+print(f"The average of the numbers is {total / len(numbers)}")
