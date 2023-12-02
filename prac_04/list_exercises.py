4b3dce690ccdc472e17f4e50d0bb505bd09d8a26
Baicha <pornnatcha.sarujikomjornwattana@my.jcu.edu.au>
2023-12-03 00:32:38 +0800

    Complete the list_exercise program.

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,12 +1,19 @@
 PROMPT_INPUT = 5
 numbers = []
-total = 0
 for i in range(0, PROMPT_INPUT):
     number = int(input("Number: "))
     numbers.append(number)
-    total += number
+
 print(f"The first number is {numbers[0]}")
 print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
-print(f"The average of the numbers is {total / len(numbers)}")
+print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access Denied")
