61a3c10608588390c35a57ca27a52559650c11b8
Baicha <pornnatcha.sarujikomjornwattana@my.jcu.edu.au>
2023-12-17 16:34:45 +0800

    Complete save_project function.

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -3,8 +3,8 @@ Project Management Program.
 Estimate time: 1 hour
 """
 
-from datetime import datetime
 from project import Project
+import datetime
 
 FILENAME = "projects.txt"
 MENU = "-(L)oad Project\n" \
@@ -24,11 +24,11 @@ def main():
         if choice == "L":
             load_project(projects)
         elif choice == "S":
-            save_project()
+            save_project(projects)
         elif choice == "D":
             display_project(projects)
         elif choice == "F":
-            filter_project()
+            filter_project(projects)
         elif choice == "A":
             add_project()
         elif choice == "U":
@@ -41,7 +41,7 @@ def main():
 
 def load_project(projects):
     """Read the file."""
-    filename_to_load = input("Project filename: ")
+    filename_to_load = input("Project filename to load: ")
     with open(filename_to_load, 'r') as in_file:
         in_file.readline()
         for line in in_file:
@@ -65,12 +65,26 @@ def display_project(projects):
             print(project)
 
 
-def save_project():
-    filename_to_save = input("")
+def save_project(projects):
+    """Write parts of the project to the file."""
+    filename_to_save = input("Project filename to save: ")
+    with open(filename_to_save, "a") as out_file:
+        print("Name\tStart Date\tPriority\tCost Estimate\tCompletion Percentage", file=out_file)
+        for project in projects:
+            print(f"{project.name}\t {project.start_date}\t {project.priority}\t "
+                  f"{project.cost_estimate}\t {project.completion_percentage}", file=out_file)
 
 
-def filter_project():
-    pass
+def filter_project(projects):
+    date_string = input("Show projects that start after date (dd/mm/yy):")  # e.g., "30/9/2022"
+    date = datetime.datetime.strptime(date_string, "%d/%m/%Y").date()
+
+    projects.sort()
+    for project in projects:
+        print(project)
+
+    # print(f"That day is/was {date.strftime('%A')}")
+    # print(date.strftime("%d/%m/%Y"))
 
 
 def add_project():
